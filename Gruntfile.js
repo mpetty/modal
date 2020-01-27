ae8394332158e6efaf08966fb5595426a4979443
Mitchell Petty <mpetty002@gmail.com>
2020-01-27 14:36:48 -0500

    revert sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,7 +54,6 @@ module.exports = function(grunt) {
 		sass: {
 			dist: {
 				options: {
-					implementation: require('node-sass'),
 					style: 'compressed',
 					sourcemap: 'none',
 					compass: false,
@@ -94,8 +93,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	// grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', [
